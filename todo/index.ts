1a7a46c2cfbec7850695607439ad9f6a0f9e1d24
emmanueljucaban <emmanueljucaban@gmail.com>
2021-01-10 00:31:27 -0800

    F - Added code to allow communication between 2 servers

--- todo/index.ts
+++ todo/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { getModelForClass, prop } from '@typegoose/typegoose';
 import { connect } from 'mongoose';
-
+import axios from 'axios';
 
 class Todo {
   @prop()
@@ -10,12 +10,10 @@ class Todo {
 
 const TodoModel = getModelForClass(Todo);
 
+
 connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('pro', process.env.MONGODB_URI);
-    if (process.env.LALA === 'LEET') {
-      console.log('YEEE');
-    }
   })
   .catch(e => console.log(e));
 
@@ -23,15 +21,23 @@ const app = express();
 
 app.get('/', async (req, res) => {
 
-  const newTodo = await TodoModel.create({
-    todo: 'Hello World'
-  });
+  // console.log('hit!')
+  // const newTodo = await TodoModel.create({
+  //   todo: 'Hello World'
+  // });
+  //
+  // const todos = await TodoModel.find({});
 
-  const todos = await TodoModel.find({});
+  try {
+    // console.log('we are the todos', todos);
+    const resp = await axios.get('http://user-server-cluster-ip-service:3001');
+    // console.log(resp.data);
+    res.json(resp.data);
+  } catch (e) {
+    res.json(e);
+  }
 
-  console.log('we are the todos', todos);
 
-  res.json(newTodo);
 });
 
 
--- user/index.ts
+++ user/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-
-const port = 3001;
+import { connect } from 'mongoose';
 
 
 const app = express();
 
-app.listen(port, () => console.log('yee'));
+app.get('/', async (req, res) => {
+  res.json('Manny you made it!');
+});
+
+
+app.listen(3001, () => console.log(`we out of here yee`));
+
