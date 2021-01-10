9754a6487bdc0b14ab73d4e09e578abf13087925
emmanueljucaban <emmanueljucaban@gmail.com>
2021-01-09 18:24:50 -0800

    F - Added cluster ip services and deployments for all containers

--- todo/index.ts
+++ todo/index.ts
@@ -1,12 +1,35 @@
 import express from 'express';
+import { getModelForClass, prop } from '@typegoose/typegoose';
+import { connect } from 'mongoose';
 
 const port = 3002;
 
+class Todo {
+  @prop()
+  todo: string;
+}
+
+const TodoModel = getModelForClass(Todo);
+
+connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected');
+  })
+  .catch(e => console.log(e));
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('yeee we out here');
+app.get('/', async (req, res) => {
+
+  const newTodo = await TodoModel.create({
+    todo: 'Hello World'
+  });
+
+  const todos = await TodoModel.find({});
+
+  console.log('we are the todos', todos);
+
+  res.json(newTodo);
 });
 
 
